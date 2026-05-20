57bb3d75d464d3f17776f6d23b1e45cf87a261fe
ba1chev <baychevyoan@gmail.com>
2026-05-20 21:14:15 +0300

    Threaded variable collection recursion through both branches of binary nodes

--- source/expressions/binary_expressions/binary_expression.py
+++ source/expressions/binary_expressions/binary_expression.py
@@ -5,3 +5,7 @@ class BinaryExpression(Expression):
     def __init__(self, left_expr: Expression, right_expr: Expression) -> None:
         self._left_expr: Expression = left_expr
         self._right_expr: Expression = right_expr
+
+    def _collect_variables(self, out: list, seen: set) -> None:
+        self._left_expr._collect_variables(out, seen)
+        self._right_expr._collect_variables(out, seen)
