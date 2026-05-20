83feb128468363cb28fb4099c28c9036f76998d0
ba1chev <baychevyoan@gmail.com>
2026-05-20 21:12:36 +0300

    Equipped Expression with variable discovery and bulk gradient zeroing primitives

--- source/expressions/expression.py
+++ source/expressions/expression.py
@@ -5,7 +5,21 @@ class Expression(ABC):
     @abstractmethod
     def forward(self) -> float:
         raise NotImplementedError("Must be implemented")
-    
+
     @abstractmethod
     def backward(self, gradient: float) -> None:
         raise NotImplementedError("Must be implemented")
+
+    @abstractmethod
+    def _collect_variables(self, out: list, seen: set) -> None:
+        raise NotImplementedError("Must be implemented")
+
+    def get_variables(self) -> list:
+        out: list = []
+        seen: set = set()
+        self._collect_variables(out, seen)
+        return out
+
+    def zero_all_gradients(self) -> None:
+        for current_variable in self.get_variables():
+            current_variable.zero_gradient()
