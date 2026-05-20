cc314270ec161abdccae675ea5cdb26575702fa9
ba1chev <baychevyoan@gmail.com>
2026-05-20 16:53:50 +0300

    Replaced evaluate with forward and backward abstract methods on Expression

--- source/expressions/expression.py
+++ source/expressions/expression.py
@@ -3,5 +3,9 @@ from abc import ABC, abstractmethod
 
 class Expression(ABC):
     @abstractmethod
-    def evaluate(self) -> float:
+    def forward(self) -> float:
+        raise NotImplementedError("Must be implemented")
+    
+    @abstractmethod
+    def backward(self, gradient: float) -> None:
         raise NotImplementedError("Must be implemented")
