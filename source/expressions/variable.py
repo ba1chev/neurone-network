c097d55827c82935648d2d0b7b7cb1e7eed810d8
ba1chev <baychevyoan@gmail.com>
2026-05-20 16:54:31 +0300

    Migrated Variable to forward and backward interface with gradient accumulation

--- source/expressions/variable.py
+++ source/expressions/variable.py
@@ -3,19 +3,34 @@ from source.expressions.expression import Expression
 
 class Variable(Expression):
     def __init__(self, value: float, name: str) -> None:
-        self._value = value
-        self._name = name
+        self._name: str = name
+        self._value: float = value
+        self._gradient: float = 0.0
 
-    def evaluate(self) -> float:
+    def forward(self) -> float:
         return self._value
+
+    def backward(self, gradient: float) -> None:
+        self._gradient += gradient
     
     @property
     def value(self) -> float:
         return self._value
     
-    @property.setter
+    @property
+    def gradient(self) -> float:
+        return self._gradient
+    
+    @value.setter
     def value(self, value: float) -> None:
         self._value = value
+
+    @gradient.setter
+    def gradient(self, gradient: float) -> None:
+        self._gradient = gradient
+
+    def zero_gradient(self):
+        self._gradient = 0.0
 
     def __repr__(self) -> str:
         return f"Var(value={self._value}, name={self._name})"
