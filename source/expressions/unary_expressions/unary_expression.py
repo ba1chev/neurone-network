8d8d5e87a8a4eeb3cf9b948709b05186e8eb2a44
ba1chev <baychevyoan@gmail.com>
2026-05-20 21:14:41 +0300

    Delegated variable collection from unary nodes down to their wrapped child

--- source/expressions/unary_expressions/unary_expression.py
+++ source/expressions/unary_expressions/unary_expression.py
@@ -4,3 +4,6 @@ from source.expressions.expression import Expression
 class UnaryExpression(Expression):
     def __init__(self, expr: Expression) -> None:
         self._expr: Expression = expr
+
+    def _collect_variables(self, out: list, seen: set) -> None:
+        self._expr._collect_variables(out, seen)
